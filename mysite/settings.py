ee8c1968ceb7ee9854ab6d6c94814bbf5e652b64
Hansol Lee <gksthf996@naver.com>
2023-07-20 09:47:01 +0900

    Comment 모델 어드민 사이트 등록

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = ''

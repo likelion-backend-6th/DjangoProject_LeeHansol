05242fc7093ceb4fe6de0006e9824b6714c32d2d
Hansol Lee <gksthf996@naver.com>
2023-07-20 10:41:14 +0900

    CommentForm 생성 post_comment 뷰 함수 추가 url 경로 추가

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.decorators.http import require_POST
 from django.views.generic import ListView
-from blog.forms import EmailPostForm
-from blog.models import Post
+from blog.forms import EmailPostForm, CommentForm
+from blog.models import Post, Comment
 
 
 # Create your views here.
@@ -53,3 +54,13 @@ def post_share(request, post_id):
     return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
 
 
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, 'blog/post/comment.html', {'post': post, 'form': form, 'comment': comment})
